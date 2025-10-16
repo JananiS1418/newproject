2eeccf126282b59fc0690e0b6501df0d0c57b80f
JananiS1418 <jananisomasundaram07@gmail.com>
2025-10-16 23:23:56 +0530

    Update

---
+++ Javascript/array/array.js
@@ -0,0 +1,38 @@
+let bookTitle = ["Little Women"];
+console.log([bookTitle][0]);
+
+let book_author = ["Louisa May Alcott"];
+console.log([book_author][0]);
+
+let bookId = [345,724,[456,986],788];
+console.log(bookId[2][1]);
+
+let memberName = ["Janani",14];
+console.log(memberName[0]);
+
+let member_id = [14];
+console.log(member_id);
+
+let memberDateOfBirth = [14 ,7,2003];
+console.log(memberDateOfBirth);
+
+let memberGender = ["male",["Female"]];
+console.log(memberGender[1][0]);
+
+let bookIsbn = [3456,[2345,6778],[2347,2345]];
+console.log(bookIsbn[2][1]);
+
+let issue_date = [1,10,2025];
+console.log(issue_date);
+
+let due_date = [5,10,2025];
+console.log(due_date);
+
+let returnDate = [14,10,2025];
+console.log(returnDate);
+
+let FineAmount = [67,45,[56,65]];
+console.log(FineAmount[2][1]);
+
+
+
